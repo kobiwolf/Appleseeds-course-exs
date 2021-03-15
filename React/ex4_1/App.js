822f58d46bf2ca111e5987795398a7aedca5d356
kobi wolf <kobiwolf95@gmail.com>
2021-03-15 11:25:52 +0200

    adding ex4.1 in React

---
+++ React/ex4_1/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './componets/Button/Button';
+
+function App() {
+  return (
+    <div>
+      <Button text="important" class="bold" />
+      <Button text="not important" />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/tamplet/src/componets/Button/Button.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+import './Button.css';
+function Button(props) {
+  return <button className={props.class}>{props.text}</button>;
+}
+
+export default Button;
