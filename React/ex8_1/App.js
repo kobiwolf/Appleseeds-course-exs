d7ec926148e133200d67e7ffa88f413945bfdf8e
kobi wolf <kobiwolf95@gmail.com>
2021-03-16 11:00:44 +0200

    add ex8.1 React

---
+++ React/ex8_1/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = { favoriteColor: 'blue' };
+  render() {
+    return <div></div>;
+  }
+}
+export default App;
--- React/tamplet/src/App.js
+++ React/tamplet/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
+  state = { favoriteColor: 'blue' };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ favoriteColor: 'red' });
+    }, 1000);
+  }
+  componentDidUpdate() {
+    const div = document.querySelector("[id='5']");
+    div.innerHTML = `The updated favorite color is ${this.state.favoriteColor}`;
   }
   render() {
-    return <div>WOW</div>;
+    return (
+      <div>
+        <h1>My Favorite Color Is:{this.state.favoriteColor}</h1>
+        <div id={5}></div>
+      </div>
+    );
   }
 }
 export default App;
