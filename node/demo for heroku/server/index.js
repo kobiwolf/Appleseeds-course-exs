36c51eef4a730bcd99c009e0e9894500509b1a3d
kobi wolf <kobiwolf95@gmail.com>
2021-04-18 21:19:00 +0300

    heroku

--- node/demo for heroku/server/index.js
+++ node/demo for heroku/server/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
