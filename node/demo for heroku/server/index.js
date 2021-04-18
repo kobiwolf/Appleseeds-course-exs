e726c24a62fd82edb12b975cb57f94339f259b6c
kobi wolf <kobiwolf95@gmail.com>
2021-04-18 21:16:21 +0300

    heroku

--- node/demo for heroku/server/index.js
+++ node/demo for heroku/server/index.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
   const joke = fs.readFileSync('./User.json');
   res.send(joke);
